84c5b3a682a36f42c685833a5de0a7bc36a9d7f6
gauthamvijayan <gauthamvijayan07@gmail.com>
2025-10-17 11:46:06 +0530

    folderchange+internilzation

--- weone/src/main.ts
+++ weone/src/main.ts
@@ -1,3 +1,4 @@
+import '@angular/localize';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
