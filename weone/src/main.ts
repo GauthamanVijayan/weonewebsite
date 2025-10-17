9b824021cec12c83145c28bc58933eb86db9260e
gauthamvijayan <gauthamvijayan07@gmail.com>
2025-10-17 12:05:30 +0530

    polyfilssadded

--- weone/src/main.ts
+++ weone/src/main.ts
@@ -1,4 +1,3 @@
-import '@angular/localize';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
